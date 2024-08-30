c67964b96568fa060c61b870295e355367952289
sharif-jobayed <sharif.jobayed@gmail.com>
2024-08-30 19:10:49 +0600

    ...

---
+++ exercises/0001.MapGameGraphicOptions/assets/javascripts/main.js
@@ -0,0 +1,34 @@
+
+
+// Main JavaScript
+
+/* Page Tags */
+const body = document.querySelectorAll ("body");
+const html = document.querySelectorAll ("html");
+const button = document.querySelectorAll ("button");
+const div = document.querySelectorAll ("div");
+const span = document.querySelectorAll ("span");
+const header = document.querySelectorAll ("header");
+const aside = document.querySelectorAll ("aside");
+const main = document.querySelectorAll ("main");
+const footer = document.querySelectorAll ("footer");
+const anchor = document.querySelectorAll ("a");
+const image = document.querySelectorAll ("img");
+const section = document.querySelectorAll ("section");
+const ul = document.querySelectorAll ("ul");
+const ol = document.querySelectorAll ("ol");
+const li = document.querySelectorAll ("li");
+const table = document.querySelectorAll ("table");
+const thead = document.querySelectorAll ("thead");
+const tbody = document.querySelectorAll ("tbody");
+const tr = document.querySelectorAll ("tr");
+const th = document.querySelectorAll ("th");
+const td = document.querySelectorAll ("td");
+const form = document.querySelectorAll ("form");
+const link = document.querySelectorAll ("link");
+const meta = document.querySelectorAll ("meta");
+const script = document.querySelectorAll ("script");
+const blockquote = document.querySelectorAll ("blockquote");
+const hr = document.querySelectorAll ("hr");
+const br = document.querySelectorAll ("br");
+/* Page Tags end */
