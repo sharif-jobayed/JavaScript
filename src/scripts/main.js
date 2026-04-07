7d3405ad45be2a4d89be7ad4d70b146bd4a41e35
sharif-jobayed <sharif.jobayed@gmail.com>
2026-04-08 05:28:25 +0600

    ...

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -38,16 +38,47 @@ cells.forEach((cell) => {
 	});
 });
 
+// Check if all celles are filled
+const checkDraw = () => {
+	return cells.every((cell) => cell.textContent);
+};
 
+// Check if a player has won
+const checkWin = () => {
+	const winConditions = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
 
+	return winConditions.some((condition) => {
+		const [a, b, c] = condition;
+		return (
+			cells[a].textContent &&
+			cells[a].textContent === cells[b].textContent &&
+			cells[a].textContent === cells[c].textContent
+		);
+	});
+}
 
-
-
-
-
-
-
-
-
-
-
+// Check game status after each move
+cells.forEach((cell) => {
+	cell.addEventListener(`click`, () => {
+		if (checkWin()) {
+			setTimeout(() => {
+				alert(`${currentPlayer === `X` ? `O` : `X`} wins!`);
+				cells.forEach((cell) => (cell.textContent = ``));
+			}, 100);
+		} else if (checkDraw()) {
+			setTimeout(() => {
+				alert(`It's a draw!`);
+				cells.forEach((cell) => (cell.textContent = ``));
+			}, 100);
+		}
+	});
+});
